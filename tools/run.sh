56f10b48f1c8967ed70e2a5c6fd8bfc096b70eb0
Rafael Marins <rafael.marins@me.com>
2012-03-06 12:06:53 -0300

    New load sequence and using keep-alive in hardcoded tests

--- tools/run.sh
+++ tools/run.sh
@@ -4,11 +4,11 @@
 PRG="$0"
 CWD=`pwd`
 BENCHMARK_HOME="`dirname ${PRG}`/.."
-BENCHMARK_LOAD="4 100 3 1000 2 5000 1 10000"
+BENCHMARK_LOAD="1 1 2 4 7 13 24 44 81 149 274 504 927 1705 3136 5768 10609"
 
 BASE_URL="http://localhost:8080"
 
-DSTAT_OUT='dstat.dat'
+DSTAT_OUT='system_stat.dat'
 
 DSTAT_EXEC=`which dstat`
 MAVEN_EXEC=`which mvn`
@@ -63,80 +63,76 @@ _fibonacci() #@ USAGE _fibonacci <num>
 run_hardcore_benchmark() #@ USAGE: run_hardcore_benchmark <server_name>
                          #             <no_connections> <no_concurrency>
 {
+  sleep 10 # wait a while before stressing the server
+
   context_path="/hardcore/test"
   log_benchmark "$context_path" "$1" "$2" "$3"
-
-  sleep 30 # wait a while before stressing the server
 
   # Apache Benchmarking tool
   ##
   output_file_prefix="ab_hardcoded_$1_$3"
  
-  $AB_EXEC -n $2 -c $3 -g "${CWD}/${output_file_prefix}.dat" \
-    "${BASE_URL}${context_path}" >> "${CWD}/${output_file_prefix}.out" 2>&1
+  $AB_EXEC -kr -n $2 -c $3 "${BASE_URL}${context_path}" >> "${CWD}/${output_file_prefix}.out" 2>&1
 
 }
 
 run_static_benchmark() #@ USAGE: run_static_benchmark <server_name>
                          #             <no_connections> <no_concurrency>
 {
-  context_path="/files/"
+
+  sleep 5
+  context_path="/files/file512.b"
   log_benchmark "$context_path" "$1" "$2" "$3"
 
-  sleep 30
+  $AB_EXEC -n $2 -c $3 "${BASE_URL}${context_path}" >> "${CWD}/ab_static_512b_$1_$3.out"
 
-  output_file_prefix="ab_hardcoded_16kb_$1_$3"
-  $AB_EXEC -n $2 -c $3 -g "${CWD}/${output_file_prefix}.dat" \
-    "${BASE_URL}${context_path}file16.kb" >> "${CWD}/${output_file_prefix}.out"
+  sleep 5
+  context_path="/files/file1.kb"
+  log_benchmark "$context_path" "$1" "$2" "$3"
 
-  sleep 30
+  $AB_EXEC -n $2 -c $3 "${BASE_URL}${context_path}" >> "${CWD}/ab_static_1kb_$1_$3.out"
 
-  output_file_prefix="ab_hardcoded_35kb_$1_$3"
-  $AB_EXEC -n $2 -c $3 -g "${CWD}/${output_file_prefix}.dat" \
-    "${BASE_URL}${context_path}file35.kb" >> "${CWD}/${output_file_prefix}.out" \
-     2>&1
+  sleep 5
+  context_path="/files/file16.kb"
+  log_benchmark "$context_path" "$1" "$2" "$3"
 
-  sleep 30
+  $AB_EXEC -n $2 -c $3 "${BASE_URL}${context_path}" >> "${CWD}/ab_static_16kb_$1_$3.out"
 
-  output_file_prefix="ab_hardcoded_116kb_$1_$3"
-  $AB_EXEC -n $2 -c $3 -g "${CWD}/${output_file_prefix}.dat" \
-    "${BASE_URL}${context_path}file116.kb" >> "${CWD}/${output_file_prefix}.out" \
-		 2>&1
+  sleep 5
+  context_path="/files/file256.kb"
+  log_benchmark "$context_path" "$1" "$2" "$3"
 
-  sleep 30
+  $AB_EXEC -n $2 -c $3 "${BASE_URL}${context_path}" >> "${CWD}/ab_static_256kb_$1_$3.out"
 
-  output_file_prefix="ab_hardcoded_511kb_$1_$3"
-  $AB_EXEC -n $2 -c $3 -g "${CWD}/${output_file_prefix}.dat" \
-    "${BASE_URL}${context_path}file511.kb" >> "${CWD}/${output_file_prefix}.out" \
-		 2>&1
+  sleep 5
+  context_path="/files/file512.kb"
+  log_benchmark "$context_path" "$1" "$2" "$3"
 
-  sleep 30
+  $AB_EXEC -n $2 -c $3 "${BASE_URL}${context_path}" >> "${CWD}/ab_static_512kb_$1_$3.out"
 
-  output_file_prefix="ab_hardcoded_1mb_$1_$3"
-  $AB_EXEC -n $2 -c $3 -g "${CWD}/${output_file_prefix}.dat" \
-    "${BASE_URL}${context_path}file1.mb" >> "${CWD}/${output_file_prefix}.out" \
-		 2>&1
+  sleep 5
+  context_path="/files/file1.mb"
+  log_benchmark "$context_path" "$1" "$2" "$3"
 
-  sleep 30
+  $AB_EXEC -n $2 -c $3 "${BASE_URL}${context_path}" >> "${CWD}/ab_static_1mb_$1_$3.out"
 
-  output_file_prefix="ab_hardcoded_2mb_$1_$3"
-  $AB_EXEC -n $2 -c $3 -g "${CWD}/${output_file_prefix}.dat" \
-    "${BASE_URL}${context_path}file2.mb" >> "${CWD}/${output_file_prefix}.out" \
-		 2>&1
+  sleep 10
+  context_path="/files/file8.mb"
+  log_benchmark "$context_path" "$1" "$2" "$3"
 
-  sleep 30
+  $AB_EXEC -n $2 -c $3 "${BASE_URL}${context_path}" >> "${CWD}/ab_static_8mb_$1_$3.out"
 
-  output_file_prefix="ab_hardcoded_20mb_$1_$3"
-  $AB_EXEC -n $2 -c $3 -g "${CWD}/${output_file_prefix}.dat" \
-    "${BASE_URL}${context_path}file20.mb" >> "${CWD}/${output_file_prefix}.out" \
-		 2>&1
+  sleep 10
+  context_path="/files/file64.mb"
+  log_benchmark "$context_path" "$1" "$2" "$3"
 
-  sleep 30
+  $AB_EXEC -n $2 -c $3 "${BASE_URL}${context_path}" >> "${CWD}/ab_static_64mb_$1_$3.out"
 
-  output_file_prefix="ab_hardcoded_221mb_$1_$3"
-  $AB_EXEC -n $2 -c $3 -g "${CWD}/${output_file_prefix}.dat" \
-    "${BASE_URL}${context_path}file221.mb" >> "${CWD}/${output_file_prefix}.out" \
-		 2>&1
+  sleep 10
+  context_path="/files/file128.mb"
+  log_benchmark "$context_path" "$1" "$2" "$3"
+
+  $AB_EXEC -n $2 -c $3 "${BASE_URL}${context_path}" >> "${CWD}/ab_static_128mb_$1_$3.out"
 
 }
 
@@ -145,13 +141,12 @@ run_benchmark() #@ USAGE: run_benchmark <server_name>
   printf "Running the benchmark...\n"
 
   for concurrency in $BENCHMARK_LOAD; do
-		_fibonacci $concurrency
-    run_hardcore_benchmark $1 $(( $concurrency * 30 )) $concurrency
+    run_hardcore_benchmark $1 $(( $concurrency * 1024 )) $concurrency
   done
 
-  for concurrency in $BENCHMARK_LOAD; do
-    run_static_benchmark $1 $(( $concurrency * 30 )) $concurrency
-  done
+#  for concurrency in $BENCHMARK_LOAD; do
+#    run_static_benchmark $1 $(( $concurrency * 128 )) $concurrency
+#  done
 
 }
 
@@ -163,7 +158,7 @@ _do_process_status() #@ do_process_status <pid> <server_name> [interval]
   logfile=$_GET_PS_LOGFILE
 
   (while true; do ps -p $1 -o comm,nlwp,%cpu,%mem,vsz,time --no-headers \
-      >> $logfile; sleep ${3:-10}; done ) &
+      >> $logfile; sleep ${3:-1}; done ) &
 
   _DO_PROCESS_STATUS=$!
 }
@@ -180,8 +175,7 @@ doc_root="${BENCHMARK_HOME}/static"
 # page for more details about the options !
 ##
 
-$DSTAT_EXEC -tm -c -d --aio -n -N lo,total --integer \
-    --output $DSTAT_OUT >> /dev/null &
+$DSTAT_EXEC -tm -c -d --aio -n -N lo,total --integer --output $DSTAT_OUT >> /dev/null &
 dstat_pid=$!
 
 #
@@ -204,7 +198,7 @@ ps_pid=$_DO_PROCESS_STATUS
 run_benchmark "netty"
 
 kill $ps_pid $netty_pid # stops Netty
-sleep 60
+sleep 30
 
 #
 # Start/stop Node.js HTTP server and run the benchmark
